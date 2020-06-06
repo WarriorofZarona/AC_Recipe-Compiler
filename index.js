ad8fb66dd6fdbb4f4ae7b7a044ebb290f81fc507
Andrew Bautista <andrewbautista85@gmail.com>
2020-06-06 19:32:38 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const recipes = require("./recipes.json")
+const items = require("./items.json")
+
+// Compare recipes json with items json, create a new json which includes data from the recipes json that includes the images from the items json
+
