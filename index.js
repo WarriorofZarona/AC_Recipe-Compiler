3285970e4d685b13a4cdbb174cd1e6184f283225
Andrew Bautista <andrewbautista85@gmail.com>
2020-06-06 20:12:01 -0400

    added JSON to variables

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const recipes = require("./recipes.json")
-const items = require("./items.json")
+const fs = require('fs');
 
+const recipes = JSON.parse(fs.readFileSync('recipes.json', 'utf8'))
+const items = JSON.parse(fs.readFileSync('items.json', 'utf8'))
 // Compare recipes json with items json, create a new json which includes data from the recipes json that includes the images from the items json
+
+const newRecipesArr = []
+
 
